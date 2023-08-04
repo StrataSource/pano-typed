08329e837087548208e5516b16ea49fdc6a8a8e7
koerismo <56563658+koerismo@users.noreply.github.com>
2023-08-04 13:02:39 -0400

    Fix builds again?

--- compile.js
+++ compile.js
@@ -1,28 +1,35 @@
-import { createReadStream, createWriteStream, readFileSync } from 'fs';
-import { glob } from 'glob';
+import { createReadStream, createWriteStream, readFileSync, readdirSync } from 'fs';
 import MultiStream from 'multistream';
+import ReplaceStream from 'replacestream';
 
 const header = readFileSync('src/header.d.ts', { encoding: 'utf-8' })
 	.replace('$npm_package_version$', process.env.npm_package_version);
 
-function build(files, out_name) {
+function build(files, out_name, replace_chaos=false) {
 	const output = createWriteStream(`dist/${out_name}.d.ts`, { encoding: 'utf-8' });
 	output.write(header);
 
 	const streams = files.map(x => createReadStream(x, { encoding: 'utf-8' }));
-	new MultiStream(streams).pipe(output);
+	const ms = new MultiStream(streams);
+
+	if (!replace_chaos) ms.pipe(output);
+	else ms.pipe(ReplaceStream('Chaos', 'CSGO')).pipe(output);
 }
 
-async function eval_globs(sources) {
+function list_files(sources) {
 	const files = [];
-
-	for (const source of sources)
-		files.push(...await glob(`src/${source}/*.d.ts`));
-
-	return files;
+	for (const source of sources) {
+		const root_dir = `src/${source}/`;
+		const rawlist = readdirSync(root_dir, { recursive: true });
+		const remapped = rawlist.map(x => root_dir+x.replace(/\\\\/, '/'));
+		files.push(...remapped);
+	}
+	return files.filter(x => x.endsWith('.d.ts'));
 }
 
 if (process.argv.length !== 4) throw('Argument count must equal 4!');
 const sources = process.argv[2].split(',');
-const files = await eval_globs(sources);
-build(files, process.argv[3]);
+const files = list_files(sources);
+
+const filename = process.argv[3];
+build(files, filename, filename === 'csgo');
