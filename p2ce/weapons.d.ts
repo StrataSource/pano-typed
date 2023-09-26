8ee9c154b263311581b15ae7a8dcce1c6ffcd35a
koerismo <56563658+koerismo@users.noreply.github.com>
2023-09-25 22:28:14 -0400

    feat: Update types for current API

--- p2ce/weapons.d.ts
+++ p2ce/weapons.d.ts
@@ -4,44 +4,67 @@
  */
 
 declare interface GlobalEventNameMap {
-	'PlayerDrop':						(weaponClass: string) => void,
-	'PlayerShoot':						(weaponIndex: number) => void,
-	'PlayerPickup':						(weaponIndex: number) => void,
-	'PlayerSwitched':					(newIndex: number, oldIndex: number) => void,
-	'PlayerAPI.SwitchToWeapon':			(weaponIndex: number) => void,
+	'WeaponStateChange':		(mode: WeaponStateMode, index: int32) => void,
+	'WeaponSelect':				(action: WeaponSelectAction) => void,
 }
 
-interface WeaponInfo {
-	classname: string;
-	slot: number;				// Inventory slot occupied - From the weapon script
-	position: number;			// Position occupied - From the weapon script (slightly hl2-specific)
-	type: 'gun' | 'melee';		// Can be either melee or gun (for now)
-	primary: {
-		usesPrimary: boolean;	// True when this weapon uses primary ammo
-		usesClips: boolean;		// True when this weapon uses clips
-		clipSize: number;		// The clip size of the weapon
-		maxAmmo: number;		// The max amount of primary ammo
-		ammo: number;			// The actual ammo count for the weapon right now
-		ammoType: string;		// A string representing the ammo type
-	};
-	secondary: {
-		usesSecondary: boolean;	// True when this weapon uses secondary ammo
-		usesClips: boolean;		// True when this weapon uses clips
-		clipSize: number;		// The clip size of the weapon
-		maxAmmo: number;		// The max amount of primary ammo
-		ammo: number;			// The actual ammo count for the weapon right now
-		ammoType: string;		// A string representing the ammo type
-	};
+declare type WeaponStateMode = ValueOf<WeaponStateModeEnum>;
+interface WeaponStateModeEnum {
+    Switch: 0,
+    Pickup: 1,
+    Drop: 2,
 }
 
-declare namespace WeaponAPI {
-	function GetWeaponCount(): uint32;
-	function GetWeapon(index: uint32): string;
-	function GetActiveWeapon(): string;
-	function DropWeapon(index: uint32): boolean;
-	function CanSwitchToWeapon(index: uint32): boolean;
-	function HasWeapon(name: string): boolean;
-	function SwitchToWeapon(index: uint32): boolean;
-	function GetWeaponIndex(name: string): uint32;
-	function GetWeaponInfo(index: uint32): WeaponInfo;
+declare type WeaponSelectAction = ValueOf<WeaponSelectActionEnum>;
+interface WeaponSelectActionEnum {
+    Next: 0,
+    Prev: 1,
+    Show: 2,
+    Hide: 3,
+}
+
+interface Weapon {
+    name: string;
+    classname: string;
+    viewmodel: string;
+    playermodel: string;
+
+    slot: number;
+    position: number;
+
+    type: 'melee' | 'gun';
+
+    primary: {
+        usesPrimary: boolean;		// True when this weapon uses primary ammo
+        usesClips: boolean;			// True when this weapon uses clips
+        clipSize: number;			// The clip size of the weapon
+        clipAmmo: number;			// The ammo in the current clip
+        ammoType: string;			// The name of the ammo, or an empty string if no name given
+        maxAmmo: number;			// The max amount of primary ammo
+        ammo: number;				// The amount of ammo remaining
+    };
+
+    secondary: {
+        usesSecondary: boolean;		// True when this weapon uses secondary ammo
+        usesClips: boolean;			// True when this weapon uses clips
+        clipSize: number;			// The clip size of the weapon
+        clipAmmo: number;			// The ammo in the current clip
+        ammoType: string;			// The name of the ammo, or an empty string if no name given
+        maxAmmo: number;			// The max amount of secondary ammo
+        ammo: number;				// The amount of ammo remaining
+    };
+}
+
+declare namespace WeaponsAPI {
+    function GetWeaponCount(): uint32;
+    function GetWeapons(): (Weapon | null)[];
+    function GetActiveWeapon(): uint32;
+    function HasWeaponClass(classname: string): boolean;
+    function HasWeapon(id: uint32): boolean;
+    function GetWeaponInfo(id: uint32): Weapon | null;
+    function GetActiveWeaponInfo(): Weapon | null;
+    function CanSwitchToWeapon(id: uint32): boolean;
+    function SwitchToWeapon(id: uint32): boolean;
+    function GetWeaponIndexFromClass(classname: string): uint32;
+    function DropWeapon(id: uint32, throwVecX: number, throwVecY: number, throwVecZ: number): boolean;
 }
