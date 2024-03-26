424ae8a5e9876088775065f087d66607abe400d2
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-03-25 23:17:24 -0700

    feat: Add the GameStateAPI

--- p2ce/index.d.ts
+++ p2ce/index.d.ts
@@ -5,3 +5,11 @@
 
 /// <reference path="../shared/index.d.ts" />
 /// <reference path="./weapons.d.ts" />
+
+declare namespace GameStateAPI {
+	/** Returns true if this is a playtest build of P2CE */
+	function IsPlaytest(): boolean;
+
+	/** Returns true if the current playtest session is recording a demo */
+	function IsPlaytestRecording(): boolean;
+}
