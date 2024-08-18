328f0f8064c076962bf614959dfa0ac7329caf9e
tsa96 <tom@trumpetdust.org>
2024-08-18 01:22:36 +0100

    refactor: optionally allow banning untyped panels and events
    I want this for Momentum, don't know if P2:CE wants it, so did a weird
    little toggle thing.

--- p2ce/index.d.ts
+++ p2ce/index.d.ts
@@ -15,3 +15,15 @@ declare namespace GameStateAPI {
 	/** Returns true if the current playtest session is recording a demo */
 	function IsPlaytestRecording(): boolean;
 }
+
+/**
+ * Whether to throw type errors when trying to register an event handler with an
+ * event that's not documented in TypeScript.
+ */
+type ALLOW_MISSING_EVENTS = false;
+
+/**
+ * Whether to throw type errors when trying to use $.CreatePanel or other API methods
+ * taking a panel name, when that panel name is not documented in TypeScript.
+ */
+type ALLOW_MISSING_PANELS = false;
