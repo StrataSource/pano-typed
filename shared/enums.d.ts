6a59c6c9dfd7aacf9e5b718bbe83565980f590c8
tsa96 <tom@trumpetdust.org>
2024-08-18 01:13:26 +0100

    refactor: replace interfaces with const enums

--- shared/enums.d.ts
+++ shared/enums.d.ts
@@ -1,22 +1,24 @@
-/** Defines a panel event source. */
-declare type PanelEventSource = ValueOf<PanelEventSourceEnum>;
-/** @group enum */
-interface PanelEventSourceEnum {
-	PROGRAM:	0;
-	GAMEPAD: 	1;
-	KEYBOARD: 	2;
-	MOUSE: 		3;
-	INVALID: 	4;
+/**
+ * Defines a panel event source.
+ * @group enum
+ */
+declare const enum PanelEventSource {
+	PROGRAM =		'program',
+	GAMEPAD =		'gamepad',
+	KEYBOARD =		'keyboard',
+	MOUSE =			'mouse',
+	INVALID =		'invalid'
 }
 
-/** Defines the current game state. */
-declare type GameUIState = ValueOf<GameUIStateEnum>;
-/** @group enum */
-interface GameUIStateEnum {
-	INVALID: 		0;
-	LOADINGSCREEN: 	1;
-	INGAME: 		2;
-	MAINMENU: 		3;
-	PAUSEMENU: 		4;
-	INTROMOVIE: 	5;
+/**
+ * Defines the current game state.
+ * @group enum
+ */
+declare const enum GameUIState {
+	INVALID =		0,
+	LOADINGSCREEN =	1,
+	INGAME =		2,
+	MAINMENU =		3,
+	PAUSEMENU =		4,
+	INTROMOVIE =	5
 }
