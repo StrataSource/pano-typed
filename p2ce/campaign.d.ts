01cce53468769660b39ae2a8cf6884dace19935b
ozxybox <kieferstashu@gmail.com>
2025-10-18 13:49:15 -0700

    More stuff

--- p2ce/campaign.d.ts
+++ p2ce/campaign.d.ts
@@ -20,4 +20,14 @@ interface CampaignInfo {
 /** [API not finalized] The Campaign API. Exclusive to P2:CE! */
 declare namespace CampaignAPI {
     function GetAllCampaigns(): CampaignInfo[];
+
+    function GetBackgroundLevel(): string;
+    function GetBackgroundMovie(): string;
+    function GetBackgroundMusic(): string;
+    function GetBackgroundImage(): string;
+
+    function SetActiveCampaign(name: string): boolean;
+    function StartCampaign(campaign: string, chapter: string): boolean;
+
+    function GetCampaignUnlockProgress(campaign: string): number;
 }
