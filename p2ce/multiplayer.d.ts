8a6f5694fe8597a13d557ab842d974a84b99bf0c
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-08-06 07:23:48 -0500

    feat: add initial set of lobby API methods

--- p2ce/multiplayer.d.ts
+++ p2ce/multiplayer.d.ts
@@ -3,30 +3,31 @@
  * P2:CE Multiplayer API.
  */
 
-declare const enum Team
-{
-    TEAM_ANY = -2,
-    TEAM_INVALID,
-    TEAM_UNASSIGNED,
-    TEAM_SP,
-    TEAM_PBODY,
-    TEAM_ATLAS
+declare const enum Team {
+	TEAM_ANY = -2,
+	TEAM_INVALID,
+	TEAM_UNASSIGNED,
+	TEAM_SP,
+	TEAM_PBODY,
+	TEAM_ATLAS
 }
 
-interface PlayerInfo
-{
-    name: string; // Cached username
-    steamID: steamID; // User SteamID
-
-    host: boolean; // Player is host of the lobby
-    slotIndex: number; // Displayed UI slot, maybe not needed here and instead left to the UI
-
-    hasAllAddons?: boolean; // Has required installed addons
-
-    team?: Team; // Game team
+interface LobbyPlayer {
+	owner: boolean;
+	id: steamID;
 }
 
 interface GlobalEventNameMap {
-    PanoramaComponent_P2CEMatchmaking_OnPlayerJoinedLobby: (xuid: steamID) => void;
-    PanoramaComponent_P2CEMatchmaking_OnPlayerLeftLobby: (xuid: steamID) => void;
+	PanoramaComponent_P2CELobby_LobbyStateChanged: () => void;
+	PanoramaComponent_P2CELobby_PlayerStateChanged: () => void;
+}
+
+declare namespace P2CELobbyAPI {
+	function IsInLobby(): boolean;
+	function CreateLobby(campaign: string): void;
+	function ChangeCampaign(campaign: string): void;
+	function OpenInviteOverlay(): boolean;
+	function ExitLobby(): void;
+	function StartGameSession(): void;
+	function GetPlayerList(): LobbyPlayer[];
 }
