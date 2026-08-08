8d0ed9ee6ce0047c829ad6f9aae791a7397c5ac9
Orsell <34631691+OrsellGit@users.noreply.github.com>
2026-08-08 15:44:09 -0700

    fix: Fixed missing parameters for kick and ban functions

--- p2ce/multiplayer.d.ts
+++ p2ce/multiplayer.d.ts
@@ -33,9 +33,9 @@ interface LobbyPlayer {
 
 interface GlobalEventNameMap {
 	PanoramaComponent_P2CELobby_LobbyStateChanged: () => void;
-	PanoramaComponent_P2CELobby_PlayerStateChanged: () => void;
-	PanoramaComponent_P2CELobby_PlayerJoined: (lobbyPlayer: LobbyPlayer) => void;
-	PanoramaComponent_P2CELobby_PlayerLeft: (lobbyPlayer: LobbyPlayer) => void;
+	PanoramaComponent_P2CELobby_PlayerStateChanged: () => void;  // Fired when when a player has joined or left the lobby.
+	PanoramaComponent_P2CELobby_PlayerJoined: (lobbyPlayer: LobbyPlayer) => void; // Fired specifically when player has joined the lobby.
+	PanoramaComponent_P2CELobby_PlayerLeft: (lobbyPlayer: LobbyPlayer) => void; // Fired specifically when player has left the lobby.
 	PanoramaComponent_P2CELobby_OnStartWithAddonsMissing: () => void;
 	PanoramaComponent_P2CELobby_OnClientJoiningGame: () => void;
 }
@@ -48,10 +48,10 @@ declare namespace P2CELobbyAPI {
 	function ExitLobby(): void;
 	function StartGameSession(): void;
 	function GetPlayerList(): LobbyPlayer[];
-	function GetMissingAddons(): bigint[];
+	function GetMissingAddons(): bigint[]; // Returns a list of workshop AppIDs the user currently doesn't have.
 	function GetCampaignID(): string;
-	function IsLobbyOwner(): boolean;
-	function KickPlayer(): void;
-	function BanPlayer(): void;
-	function UnBanPlayer(): void;
+	function IsLobbyOwner(): boolean; // Check if the local user is the owner/host of the lobby.
+	function KickPlayer(steamID: steamID): void;
+	function BanPlayer(steamID: steamID): void;
+	function UnBanPlayer(steamID: steamID): void;
 }
