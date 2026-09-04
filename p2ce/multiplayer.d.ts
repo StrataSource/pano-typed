4ef2e12bf1d0f27b0659193333708fee626606d7
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-09-04 16:47:02 -0500

    feat: api updates things

--- p2ce/multiplayer.d.ts
+++ p2ce/multiplayer.d.ts
@@ -1,6 +1,5 @@
 /**
- * @packageDocumentation
- * P2:CE Multiplayer API.
+ * @packageDocumentation P2:CE Multiplayer API.
  */
 
 /**
@@ -21,6 +20,19 @@ declare const enum LobbyVisibility {
 	PUBLIC
 }
 
+declare const enum LobbyMemberReadyState {
+	NOT_READY = 0,
+	DOWNLOADING_ADDONS = 1,
+	READY = 2
+}
+
+declare const enum LobbyState {
+	INVALID = 0,
+	LOBBY = 1,
+	LOADING = 2,
+	PLAYING = 3
+}
+
 /**
  * Internal representation of a player in the lobby UI.
  */
@@ -28,16 +40,30 @@ interface LobbyPlayer {
 	owner: boolean; // Creator/host of the lobby.
 	id: steamID;
 	name: string;
+	state: LobbyMemberReadyState;
+	team: LobbyTeam;
+}
+
+interface LobbyData {
+	state: LobbyState;
+	campaign: string;
+	chapter: string;
+	map: number;
 }
 
 interface GlobalEventNameMap {
 	GameDisconnection: (reason: string) => void;
 	
-	PanoramaComponent_P2CELobby_LobbyStateChanged: () => void;
-	PanoramaComponent_P2CELobby_PlayerStateChanged: () => void;  // Fired when when a player's state has changed within the lobby.
+	PanoramaComponent_P2CELobby_LobbyJoinInProgress: (lobbyid: string) => void;
+	PanoramaComponent_P2CELobby_LobbyJoinFailed: (lobbyid: string, reason: string) => void;
+	PanoramaComponent_P2CELobby_LobbyJoined: (lobbyid: string) => void;
+	PanoramaComponent_P2CELobby_LobbyLeft: (lobbyid: string) => void;
+	PanoramaComponent_P2CELobby_LobbyStateChanged: (metadata: LobbyData) => void; // Lobby metadata was updated
+	PanoramaComponent_P2CELobby_PlayerStateChanged: (who: steamID, lobbyPlayer: LobbyPlayer) => void;  // Fired when when a player's state has changed within the lobby.
 	PanoramaComponent_P2CELobby_PlayerJoined: (lobbyPlayer: LobbyPlayer) => void; // Fired specifically when player has joined the lobby.
-	PanoramaComponent_P2CELobby_PlayerLeft: (lobbyPlayer: LobbyPlayer) => void; // Fired specifically when player has left the lobby.
-	PanoramaComponent_P2CELobby_OnClientJoiningGame: () => void;
+	PanoramaComponent_P2CELobby_PlayerLeft: (lobbyPlayer: steamID) => void; // Fired specifically when player has left the lobby.
+	PanoramaComponent_P2CELobby_ReadyStateChanged: (state: LobbyMemberReadyState) => void; // Local player's ready state
+	PanoramaComponent_P2CELobby_JoiningGame: () => void; // When the local player starts joining the host
 }
 
 declare namespace P2CELobbyAPI {
@@ -46,14 +72,18 @@ declare namespace P2CELobbyAPI {
 	function ChangeCampaign(campaign: string): void;
 	function OpenInviteOverlay(): boolean;
 	function ExitLobby(): void;
-	function SetReadyStatus(): void;
+	function SetReadyStatus(ready: boolean): void; // Attempt to ready up. This will start downloading addons if necessary.
 	function GetPlayerList(): LobbyPlayer[];
 	function GetCampaignID(): string;
 	function IsLobbyOwner(): boolean; // Check if the local user is the owner/host of the lobby.
 	function KickPlayer(steamID: steamID): void;
 	function BanPlayer(steamID: steamID): void;
 	function UnBanPlayer(steamID: steamID): void;
+	function GetBannedPlayers(): steamID[];
 	// Requests the current player's team to be set to the provided value.
 	// It may fail under some circumstances.
 	function SetTeam(desired: LobbyTeam): boolean;
+	function GetFriendsPlayingGame(): [ { id: string } ]; // Gets the list of friends (their Steam IDs) playing the same App ID
+	function SetCampaignChapter(chapter: string): void;
+	function SetCampaignMap(map: number): void;
 }
